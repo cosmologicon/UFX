324d44082c6067cf0283a11d971b242024b629f3
Christopher Night <cosmologicon@gmail.com>
2016-02-02 18:45:49 -0500

    UFX.glprog test pages

--- src/glprog.js
+++ src/glprog.js
@@ -15,13 +15,17 @@
 
 // Convenience functions:
 //   prog.use()
+// For uniforms:
 //   prog.uniforms.uname  -> the uniform locator for the uniform uname
 //   prog.set.uname(value)  -> set the uniform to the given value
 //     value should be a scalar or Array as appropriate to the type of the uniform:
-//     prog.set.myfloat(1)
-//     prog.set.myvec2([2, 3])
-//     prog.set.mymat2([4, 5, 6, 7])
-
+//     prog.set.myfloat(1)  -> calls gl.uniform1f
+//     prog.set.myvec2([2, 3])  -> calls gl.uniform2fv
+//     prog.set.mymat2([4, 5, 6, 7])  -> calls gl.uniformMatrix2fv
+// For attributes:
+//   prog.attribs.aname  -> the attribute location for the given attribute name
+//   prog.setgeneric.aname(value)  -> set the attribute to the generic value and disable as array
+//   prog.set.aname(buffer)  -> set the attribute to the given buffer and enable it
 
 "use strict"
 var UFX = UFX || {}
@@ -88,6 +92,116 @@ UFX.glutil = {
 			return `uniform${typesize}${typeletter}v`
 		}
 	},
+
+	// A argument-checking wrapper for functions that expect a single scalar.
+	_wrapargcheckscalar: function (func, message) {
+		return function (arg) {
+			// isNaN(arg.length) distinguishes between numbers and Arrays.
+			if (arguments.length != 1 || !isNaN(arg.length)) throw message
+			func(arg)
+		}
+	},
+	// A argument-checking wrapper for functions that expect a single Array argument of the given size.
+	_wrapargcheckarray: function (func, size, message) {
+		return function (arg) {
+			if (arguments.length != 1 || arg.length != size) {
+				throw message
+			}
+			func(arg)
+		}
+	},
+	_getuniformsetter: function (uname, location, type, count, _gl) {
+		var typesize = this.typesize(type, _gl)
+		var letter = this.typeletter(type, _gl)
+		var ismatrix = this.ismatrixtype(type, _gl)
+		var ntypevalues = ismatrix ? typesize * typesize : typesize
+		var nvalues = count * ntypevalues
+		var settername = [
+			"uniform",
+			ismatrix ? "Matrix" : "",
+			typesize,
+			letter,
+			nvalues == 1 ? "" : "v"
+		].join("")
+		var setter
+		if (ismatrix) {
+			setter = _gl[settername].bind(_gl, location, false)
+		} else {
+			setter = _gl[settername].bind(_gl, location)
+		}
+		if (true) {  // Add argument checking
+			var typename = UFX.glutil.enumasstring(type, _gl)
+			if (count > 1) typename += `[${count}]`
+			if (nvalues == 1) {
+				var message = `Setter gl.${settername} for uniform ${uname} requires a single scalar argument of type ${typename}.`
+				setter = this._wrapargcheckscalar(setter, message)
+			} else {
+				var message = `Setter gl.${settername} for uniform ${uname} requires a single length-${nvalues} Array argument of type ${typename}.`
+				setter = this._wrapargcheckarray(setter, nvalues, message)
+			}
+		}
+		return setter
+	},
+	_getattribsetter: function (aname, location, type, count, _gl) {
+		// TODO: make sure this actually works with mat3 attribs!
+		console.assert(count == 1)
+		var typesize = this.typesize(type, _gl)
+		var letter = this.typeletter(type, _gl)
+		var ismatrix = this.ismatrixtype(type, _gl)
+		var ntypevalues = ismatrix ? typesize * typesize : typesize
+		var nvalues = count * ntypevalues
+		var settername = [
+			"vertexAttrib",
+			typesize,
+			letter,
+			nvalues == 1 ? "" : "v"
+		].join("")
+		var setter = function (arg) {
+			_gl[settername](location, arg)
+			_gl.disableVertexAttribArray(location)
+		}
+		if (true) {  // Add argument checking
+			var typename = UFX.glutil.enumasstring(type, _gl)
+			if (count > 1) typename += `[${count}]`
+			if (nvalues == 1) {
+				var message = `Constant setter gl.${settername} for vertex attribute ${aname} requires a single scalar argument of type ${typename}.`
+				setter = this._wrapargcheckscalar(setter, message)
+			} else {
+				var message = `Constant setter gl.${settername} for vertex attribute ${aname} requires a single length-${nvalues} Array argument of type ${typename}.`
+				setter = this._wrapargcheckarray(setter, nvalues, message)
+			}
+		}
+		return setter
+	},
+	_getattribpointersetter: function (aname, location, type, count, _gl) {
+		console.assert(count == 1)
+		var typesize = this.typesize(type, _gl)
+		var letter = this.typeletter(type, _gl)
+		var ismatrix = this.ismatrixtype(type, _gl)
+		var ntypevalues = ismatrix ? typesize * typesize : typesize
+		var nvalues = count * ntypevalues
+		var setter = function (data) {
+			if (Array.isArray(data)) data = Float32Array.from(data)
+			var buffer = _gl.createBuffer()
+			_gl.bindBuffer(_gl.ARRAY_BUFFER, buffer)
+			_gl.bufferData(_gl.ARRAY_BUFFER, data, _gl.STATIC_DRAW)
+			_gl.enableVertexAttribArray(location)
+			// TODO: correctly handle other types than Float32Array
+			_gl.vertexAttribPointer(location, typesize, _gl.FLOAT, _gl.FALSE, 0, 0)
+		}
+		if (true) {  // Add argument checking
+			setter = (function (func) {
+				return function (arg) {
+					var isarray = Array.isArray(arg) || arg instanceof Float32Array
+					if (arguments.length != 1 || !isarray) {
+						throw `Setter for vertex attribute ${aname} requires a single argument Array or Float32Array.`
+					}
+					func(arg)
+				}
+			})(setter)
+		}
+		return setter
+	},
 }
 
 
@@ -98,7 +212,11 @@ UFX.glprog = function (vsource, fsource, _gl) {
 	this.vshader = this.createshader(vsource, this.gl.VERTEX_SHADER)
 	this.fshader = this.createshader(fsource, this.gl.FRAGMENT_SHADER)
 	this.buildprogram()
+	this.use()
+	this.set = {}
+	this.setconstant = {}
 	this.setupuniforms()
+	this.setupattribs()
 }
 UFX.glprog.prototype = {
 	createshader: function (shadersource, shadertype) {
@@ -129,67 +247,27 @@ UFX.glprog.prototype = {
 		this.gl.validateProgram(this.prog)
 		// TODO: check validation status
 	},
-
 	setupuniforms: function () {
-		function getuniformsetter(uname, location, type, count, _gl) {
-			var size = UFX.glutil.typesize(type, _gl)
-			var letter = UFX.glutil.typeletter(type, _gl)
-			var ismatrix = UFX.glutil.ismatrixtype(type, _gl)
-			var settername = [
-				"uniform",
-				ismatrix ? "Matrix" : "",
-				size,
-				letter,
-				size == 1 ? "" : "v"
-			].join("")
-			var setter
-			if (ismatrix) {
-				setter = _gl[settername].bind(_gl, location, false)
-			} else {
-				setter = _gl[settername].bind(_gl, location)
-			}
-			if (true) {  // Add argument checking
-				var typename = UFX.glutil.enumasstring(type, _gl)
-				if (size == 1) {
-					var message = `Setter gl.${settername} for uniform ${uname} requires a single scalar argument of type ${typename}.`
-					console.log(setter)
-					setter = function (method) {
-						return function (arg) {
-							if (arguments.length != 1 || arg.length) {
-								throw message
-							}
-							method(arg)
-						}
-					}(setter)
-				} else {
-					var nvalues = ismatrix ? size * size : size
-					var message = `Setter gl.${settername} for uniform ${uname} requires a single length-${nvalues} Array argument of type ${typename}.`
-					setter = function (method) {
-						return function (arg) {
-							if (arguments.length != 1 || arg.length != nvalues) {
-								throw message
-							}
-							method(arg)
-						}
-					}(setter)
-				}
-			}
-			return setter
-		}
-		this.set = {}
 		this.uniforms = {}
 		var nuniforms = this.gl.getProgramParameter(this.prog, this.gl.ACTIVE_UNIFORMS)
 		for (var i = 0 ; i < nuniforms ; ++i) {
 			var info = this.gl.getActiveUniform(this.prog, i)
 			this.uniforms[info.name] = this.gl.getUniformLocation(this.prog, info.name)
-			this.set[info.name] = getuniformsetter(info.name, this.uniforms[info.name], info.type, 1, this.gl)
+			this.set[info.name] = UFX.glutil._getuniformsetter(info.name, this.uniforms[info.name], info.type, info.size, this.gl)
 		}
-		// TODO: see what the uniform info looks like for a uniform array
-		// or for a uniform struct
+	},
+	setupattribs: function () {
+		this.attribs = {}
+		var nattribs = this.gl.getProgramParameter(this.prog, this.gl.ACTIVE_ATTRIBUTES)
+		for (var i = 0 ; i < nattribs ; ++i) {
+			var info = this.gl.getActiveAttrib(this.prog, i)
+			this.attribs[info.name] = this.gl.getAttribLocation(this.prog, info.name)
+			this.set[info.name] = UFX.glutil._getattribpointersetter(info.name, this.attribs[info.name], info.type, info.size, this.gl)
+			this.setconstant[info.name] = UFX.glutil._getattribsetter(info.name, this.attribs[info.name], info.type, info.size, this.gl)
+		}
 	},
 	use: function () {
 		this.gl.useProgram(this.prog)
 	},
-
 
 }
